0e61beb6bb5654459e2d2de5d55fa58f03955063
romheloise <romheloise@gmail.com>
2020-11-19 22:47:37 +0100

    almost working

--- src/components/display-battle/DisplayBattles.jsx
+++ src/components/display-battle/DisplayBattles.jsx
@@ -8,45 +8,67 @@ import { useEffect, useState } from "react";
 const DisplayBattles = ({ match }) => {
   const [contenders, setContenders] = useState([]);
   const [fighters, setFighters] = useState([{}, {}]);
-  const [remTime, setRemTime] = useState(20);
+  const [remTime, setRemTime] = useState(10);
 
-  const fetchContender = async () => {
+  const fetchContender = () => {
     const theme = match.params.style;
     const url = `http://9e420d11d0ce.ngrok.io/${theme}/all`;
     Axios.get(url)
       .then((res) => res.data)
       .then((data) => {
-        console.log(data)
         setContenders(data);
       });
   };
 
   useEffect(() => {
-    fetchContender().then(() => {
-      const random = [];
-      for (let index = 0; index < 2; index++) {
-        random.push(Math.floor(Math.random() * contenders.length));
-      }
-      const tempFight = [contenders[random[0]], contenders[random[1]]];
-      setFighters(tempFight);
-    });
-  }, []);
+    setRemTime(10);
+    fetchContender();
+    const random = [];
+    for (let index = 0; index < 2; index++) {
+      random.push(Math.floor(Math.random() * contenders.length));
+    }
+    const tempFight = [contenders[random[0]], contenders[random[1]]];
+    setFighters(tempFight);
+  }, [match]);
+
+  useEffect(() => {
+    setRemTime(10);
+    fetchContender();
+    const random = [];
+    for (let index = 0; index < 2; index++) {
+      random.push(Math.floor(Math.random() * contenders.length));
+    }
+    const tempFight = [contenders[random[0]], contenders[random[1]]];
+    setFighters(tempFight);
+  },[]);
 
   useEffect(() => {
     const rebours = setTimeout(() => {
-      setRemTime(parseInt(remTime) - 1);
+      setRemTime(remTime - 1);
     }, 1000);
+
     if (remTime === 0) {
-      clearInterval(rebours)
-      fetchContender();
-      setRemTime(20);
+      clearInterval(rebours);
+      setRemTime(10);
+
+      const randomOne = Math.floor(Math.random() * contenders.length - 1);
+      let randomTwo = Math.floor(Math.random() * contenders.length - 1);
+      if (randomOne === randomTwo) {
+        randomTwo = contenders.length - 1;
+      }
+      console.log(contenders.length)
+      const tempFight = [contenders[randomOne], contenders[randomTwo]];
+      setFighters(tempFight);
     }
+    return () => {
+      clearInterval(rebours);
+    };
   }, [remTime]);
 
   return (
     <div className="battles-container">
       <Contenders />
-      <p>Time Remaining : {remTime}</p>
+      <p className="timer">Time Remaining : {remTime}</p>
       <div className="battles-wrapper">
         <LeftCont {...fighters[0]} />
         <RightCont {...fighters[1]} />
