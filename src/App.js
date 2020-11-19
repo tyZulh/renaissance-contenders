9354033cef8645f52577334931174edd6e49f602
Grégory <grg.vilar@gmail.com>
2020-11-19 13:35:41 +0100

    card in progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import Card from './components/card/Card'
+
+
 
 
 function App() {
+  const info = 
+    {
+      name: "Kanye",
+      url: "https://i2-prod.mirror.co.uk/incoming/article6982466.ece/ALTERNATES/s615b/Kanye-West.jpg",
+      artist: "Kanye",
+      nbrVote: "0",
+      style: "music",
+      nationality: "USA",
+      comment: "good vibes"
+    }
+  
+
   return (
     <div className="App">
-
+      < Card {...info}/>
     </div>
   );
 }
