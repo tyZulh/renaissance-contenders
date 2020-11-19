872392c0543b1c0f2e0909109b6d8127ad97b535
romheloise <romheloise@gmail.com>
2020-11-19 12:48:50 +0100

    working nav need css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { Route, Switch } from 'react-router-dom'
+import Navigation from './components/nav/Navigation'
+
+
+const test = ({ match }) => {
+  return (
+    <div style={{ marginTop: '5em' }}>{match.params.style}</div>
+  )
+}
+
 
 
 function App() {
   return (
     <div className="App">
-
+      <Navigation />
+      <Switch>
+        <Route path="/:style" component={test} />
+      </Switch>
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/components/nav/Navigation.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import "./Navigation.scss";
+
+const Navigation = () => {
+  const menu = ["Philosophie", "Musique", "Tableaux", "Mode"];
+
+  return (
+    <nav>
+      <ul>
+        {menu.map((x) => {
+          return (
+            <Link to={x} key={x}>
+              <li>{x}</li>
+            </Link>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
