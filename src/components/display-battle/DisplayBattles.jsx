30b9fc3fb342e9d16671bdbba860f475ed7a2042
tyZulh <thomas.paoli.tp@gmail.com>
2020-12-09 20:42:46 +0100

    wtf the localhost:8000

--- src/components/display-battle/DisplayBattles.jsx
+++ src/components/display-battle/DisplayBattles.jsx
@@ -15,7 +15,7 @@ const DisplayBattles = ({ match }) => {
 
   const fetchContender = () => {
     const theme = match.params.style;
-    const url = `http://localhost:8000//${theme}/all`;
+    const url = `http://citizen-court-api.captain.citizen-court.tech/${theme}/all`;
     Axios.get(url)
       .then((res) => res.data)
       .then((data) => {
